e4c720010b5bde7f63bbb6b330690a77998ffec7
Andres Cervantes <andres.cerv.7@gmail.com>
2020-08-29 10:16:41 -0600

    initial commit

---
+++ 002_lista_de_diccionarios.py
@@ -0,0 +1,34 @@
+data = {
+    'students': [
+        {
+            'name': 'Diego',
+            'class': 2019,
+            'scores': [75, 69, 80, 45]
+        },
+        {
+            'name': 'Andres',
+            'class': 2019,
+            'scores': [91, 95, 97, 95]
+        },
+        {
+            'name': 'Ezequiel',
+            'class': 2020,
+            'scores': [75, 81, 79, 75]
+        },
+        {
+            'name': 'Katia',
+            'class': 2020,
+            'scores': [75, 78, 81, 83]
+        },
+        {
+            'name': 'Poncho',
+            'class': 2018,
+            'scores': [23, 49, 33, 40]
+        }
+    ]
+}
+
+# print(type(data))
+# print(data.get('students'))
+# print(data.get('students'), [])
+print(data['students'][0])
---
+++ README.md
@@ -0,0 +1,1 @@
+# BEDU: Data Analysis 01
